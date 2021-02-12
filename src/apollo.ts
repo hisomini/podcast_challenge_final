a5ff680ab20c21bb415d2b384367c78bbef685af
somin <qkrthals0524@naver.com>
2021-02-13 03:58:39 +0900

    끝!

--- src/apollo.ts
+++ src/apollo.ts
@@ -6,7 +6,6 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { LS_TOKEN } from "./constants";
-import { WebSocketLink } from "@apollo/client/link/ws";
 
 const token = localStorage.getItem(LS_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
@@ -18,24 +17,9 @@ export const makeLogout = () => {
   isLoggedInVar(false);
   authTokenVar(null);
 };
-const wsLink = new WebSocketLink({
-  uri:
-    process.env.NODE_ENV === "production"
-      ? "wss://podcast--backend.herokuapp.com/graphql"
-      : `ws://localhost:4000/graphql`,
-  options: {
-    reconnect: true,
-    connectionParams: {
-      "x-jwt": authTokenVar() || "",
-    },
-  },
-});
 
 const httpLink = createHttpLink({
-  uri:
-    process.env.NODE_ENV === "production"
-      ? "https://podcast--backend.herokuapp.com/graphql"
-      : "http://localhost:4000/graphql",
+  uri: "https://podcast--backend.herokuapp.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
